6aaf2c72d9f308d8243655aed29f283f73b0d55d
Bryan DeGrendel <bryan@degrendel.com>
2019-11-14 00:12:03 -0500

    Implement all Kotlin side parsing until bodies

--- gliding-soar-service/src/main/kotlin/com/degrendel/glidingsoar/service/BlockParser.kt
+++ gliding-soar-service/src/main/kotlin/com/degrendel/glidingsoar/service/BlockParser.kt
@@ -1,12 +1,12 @@
 package com.degrendel.glidingsoar.service
 
-import com.degrendel.glidingsoar.common.ast.Element
-import com.degrendel.glidingsoar.common.ast.Location
+import com.degrendel.glidingsoar.common.ast.*
 import com.degrendel.glidingsoar.service.grammar.GlidingSoarBaseVisitor
 import com.degrendel.glidingsoar.service.grammar.GlidingSoarLexer
 import com.degrendel.glidingsoar.service.grammar.GlidingSoarParser
 import org.antlr.v4.runtime.*
 import org.antlr.v4.runtime.misc.ParseCancellationException
+import org.antlr.v4.runtime.tree.TerminalNode
 
 sealed class ParserResults
 data class ParseSuccess(val elements: List<Element>) : ParserResults()
@@ -47,13 +47,49 @@ class BlockParser : GlidingSoarBaseVisitor<List<Element>>()
     }
   }
 
-  override fun visitGlidingSoar(ctx: GlidingSoarParser.GlidingSoarContext): List<Element> = ctx.element().map { elementVisitor.visitElement(it) }
+  override fun visitGlidingSoar(ctx: GlidingSoarParser.GlidingSoarContext): List<Element> = ctx.element().map { visitor.visitElement(it) }
 
-  private val elementVisitor = object : GlidingSoarBaseVisitor<Element>()
+  private val visitor = object : GlidingSoarBaseVisitor<ASTNode>()
   {
-    override fun visitElement(ctx: GlidingSoarParser.ElementContext?): Element
+    override fun visitElement(ctx: GlidingSoarParser.ElementContext): Element
     {
-      TODO("Stub!")
+      val declaration = visitDeclaration(ctx.declaration())
+      val body = visitBody(ctx.body())
+      return Element(symbolToLocation(ctx.start), declaration, body)
     }
+
+    override fun visitDeclaration(ctx: GlidingSoarParser.DeclarationContext): Declaration
+    {
+      val identifier = convertIdentifier(ctx.IDENTIFIER())
+      val type = visitType(ctx.type())
+      val extends = if (ctx.extends_() == null)
+        ArrayList<Identifier>()
+      else
+        ctx.extends_().IDENTIFIER().map { convertIdentifier(it) }
+      return Declaration(symbolToLocation(ctx.start), type, identifier, extends)
+    }
+
+    override fun visitType(ctx: GlidingSoarParser.TypeContext): Type
+    {
+      return when
+      {
+        ctx.INPUT() != null -> Input(symbolToLocation(ctx.INPUT().symbol))
+        ctx.OBJECT() != null -> Object(symbolToLocation(ctx.OBJECT().symbol))
+        ctx.OUTPUT() != null -> Output(symbolToLocation(ctx.OUTPUT().symbol))
+        ctx.INTERFACE() != null -> Interface(symbolToLocation(ctx.INTERFACE().symbol))
+        else -> throw IllegalStateException("Found type $ctx that isn't input/object/output/interface")
+      }
+    }
+
+    override fun visitBody(ctx: GlidingSoarParser.BodyContext): Body
+    {
+      // TODO: When AST supports it, actually parse the body
+      return Body(symbolToLocation(ctx.CLOSE_CURLY().symbol))
+    }
+
+    fun convertIdentifier(identifier: TerminalNode) = Identifier(symbolToLocation(identifier.symbol), identifier.symbol.text)
   }
+
+
+  fun symbolToLocation(symbol: Token) = Location(source.line + symbol.line, symbol.charPositionInLine)
 }
