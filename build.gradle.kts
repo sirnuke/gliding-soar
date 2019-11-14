521fb6471d3e8b27f8ac412b347d4d441ec9ae18
Bryan DeGrendel <bryan@degrendel.com>
2019-11-14 00:10:46 -0500

    Set compilation to 1.8
    Was defaulting to 1.6?  What year is it?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   base
   kotlin("jvm") version "1.3.50" apply false
@@ -28,5 +30,9 @@ subprojects {
   dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.slf4j:slf4j-simple:1.7.25")
+  }
+
+  tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
   }
 }
