ac9d6dc5fd309ad3e0bb676a6f3a745be5af5c94
Bryan DeGrendel <bryan@degrendel.com>
2019-11-26 16:08:44 -0500

    Add test cases for parameter and member parsing

--- gliding-soar-service/src/test/kotlin/com/degrendel/glidingsoar/service/BlockParserTests.kt
+++ gliding-soar-service/src/test/kotlin/com/degrendel/glidingsoar/service/BlockParserTests.kt
@@ -1,11 +1,8 @@
 package com.degrendel.glidingsoar.service
 
-import com.degrendel.glidingsoar.common.ast.Input
-import com.degrendel.glidingsoar.common.ast.Interface
-import com.degrendel.glidingsoar.common.ast.Location
-import com.degrendel.glidingsoar.common.ast.Output
-import com.degrendel.glidingsoar.common.ast.Object
+import com.degrendel.glidingsoar.common.ast.*
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -42,6 +39,33 @@ class BlockParserTests
     }
   }
 
+  private fun assertFailure(block: String): ParseFailure
+  {
+    return when (val result = parser.parse(location, block))
+    {
+      is ParseFailure -> result
+      is ParseSuccess -> fail("Expected parse failure!")
+    }
+  }
+
+  private fun assertParameter(param: Parameter, name: String, type: String, optional: Boolean = false, multiple: Boolean = false)
+  {
+    assertEquals(name, param.identifier.value)
+    assertEquals(type, param.type.value)
+    assertEquals(optional, param.optional)
+    assertEquals(multiple, param.multiple)
+  }
+
+  private fun assertMember(member: Member, name: String, type: String, i_support: Boolean = true, tag: Boolean = false, multiple: Boolean = false)
+  {
+    assertEquals(name, member.identifier.value)
+    assertEquals(type, member.type.value)
+    val klass = if (i_support) IMember::class else OMember::class
+    assertEquals(klass, member::class)
+    assertEquals(tag, member.tag)
+    assertEquals(multiple, member.multiple)
+  }
+
   @ParameterizedTest
   @ValueSource(strings = ["", "\t", "\n\n", "  \n\t  \n\n", "# comment!\n", "// alt comment!", " # another", "\t//yeah"])
   fun `Various no element strings parse and result in zero elements`(block: String)
@@ -72,5 +96,46 @@ class BlockParserTests
     """.trimIndent()
     val element = assertSuccess(block).elements.first()
     assertEquals(name, element.declaration.identifier.value)
+  }
+
+
+  @Test
+  fun `Parses object parameters`()
+  {
+    val block = """
+      object FooBar {
+        param foo: Boolean
+        param bar: FooBar
+        param hello: Int?
+        param world: Thing+
+        param what: String+?
+      }
+    """.trimIndent()
+    val body = assertSuccess(block).elements.first().body
+    assertEquals(5, body.parameters.size)
+    assertParameter(body.parameters[0], "foo", "Boolean")
+    assertParameter(body.parameters[1], "bar", "FooBar")
+    assertParameter(body.parameters[2], "hello", "Int", optional = true)
+    assertParameter(body.parameters[3], "world", "Thing", multiple = true)
+    assertParameter(body.parameters[4], "what", "String", optional = true, multiple = true)
+  }
+
+  @Test
+  fun `Parses object members`()
+  {
+    val block = """
+      object FooBar {
+        i foo: String
+        o bar: Thing
+        i hello: Int+
+        o tag world: Tag
+      }
+    """.trimIndent()
+    val body = assertSuccess(block).elements.first().body
+    assertEquals(4, body.members.size)
+    assertMember(body.members[0], "foo", "String")
+    assertMember(body.members[1], "bar", "Thing", i_support = false)
+    assertMember(body.members[2], "hello", "Int", multiple = true)
+    assertMember(body.members[3], "world", "Tag", i_support = false, tag = true)
   }
 }
