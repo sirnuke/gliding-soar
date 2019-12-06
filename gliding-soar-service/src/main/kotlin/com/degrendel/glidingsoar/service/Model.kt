4d714a8cb339616da0df6d1648514d3a3236b083
Bryan DeGrendel <bryan@degrendel.com>
2019-12-06 02:41:41 -0500

    Commit broken organization plus steps towards models

---
+++ gliding-soar-service/src/main/kotlin/com/degrendel/glidingsoar/service/Model.kt
@@ -0,0 +1,81 @@
+package com.degrendel.glidingsoar.service
+
+import com.degrendel.glidingsoar.common.ValidationIssue
+import com.degrendel.glidingsoar.common.ast.Element
+import com.degrendel.glidingsoar.common.ast.Location
+import org.stringtemplate.v4.STGroupFile
+import java.time.Instant
+
+fun main()
+{
+  val model = Model()
+  println(model.bundle())
+}
+
+class Model
+{
+  private val elements = ArrayList<Element>()
+  private val template = STGroupFile(javaClass.getResource("/templates/template.stg"))
+
+  fun addElements(elements: List<Element>)
+  {
+    this.elements.addAll(elements)
+  }
+
+  fun bundle(): String
+  {
+    val bundle = template.getInstanceOf("bundle")
+    bundle.add("model", this)
+    bundle.add("version", "TODO")
+    bundle.add("when", Instant.now())
+    return bundle.render()
+  }
+
+  fun parseString(contents: String): ParserResults
+  {
+    // TODO: This is kinda painful
+    // Being able to filter content on a start/stop predicate feels like it should be possible, but maybe not in Kotlin?
+    // Could probably also adapt the grammar to ignore non comments and whatnot
+    // Could also also turn this into a single monster multiline regex but Regex: now you have two problems
+    val start = Regex("^\\s*#\\s*<glide>\\s*$")
+    val content = Regex("^\\s*#\\s*(?<content>.*)$")
+    val stop = Regex("^\\s*#\\s*</glide>\\s*$")
+    val results = ArrayList<Element>()
+    val body = ArrayList<String>()
+    var inBody = false
+    val parser = BlockParser()
+    contents.lines().forEachIndexed { index, line ->
+      when (inBody)
+      {
+        false -> if (start.matches(line)) inBody = true
+        true ->
+        {
+          if (stop.matches(line))
+          {
+            inBody = false
+            when (val result = parser.parse(Location(index, 0), body.joinToString(separator = "\n")))
+            {
+              is ParseSuccess -> results.addAll(result.elements)
+              is ParseFailure -> return result
+            }
+          }
+          else
+          {
+            val inner = content.find(line)
+            if (inner != null)
+              body.add(inner.groups["content"]!!.value)
+            else
+              return ParseFailure(Location(index + 1, 0), "Unexpected line, expected line of content or </glide>")
+          }
+        }
+      }
+    }
+    return ParseSuccess(results)
+
+  }
+
+  fun validate(): List<ValidationIssue>
+  {
+    TODO("Stub!")
+  }
+}
---
+++ gliding-soar-service/src/test/kotlin/com/degrendel/glidingsoar/service/ModelTests.kt
@@ -0,0 +1,5 @@
+package com.degrendel.glidingsoar.service
+
+class ModelTests
+{
+}
