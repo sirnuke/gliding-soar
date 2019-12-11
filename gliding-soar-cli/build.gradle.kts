0c61f41e1ed9aae33efc0081f120e05df3f40d4d
Bryan DeGrendel <bryan@degrendel.com>
2019-12-11 10:35:20 -0500

    Package CLI module as an application

--- gliding-soar-cli/build.gradle.kts
+++ gliding-soar-cli/build.gradle.kts
@@ -1,3 +1,11 @@
+plugins {
+  application
+}
+
+application {
+  mainClassName = "com.degrendel.glidingsoar.cli.MainKt"
+}
+
 dependencies {
   implementation("info.picocli:picocli:4.1.1")
   implementation(project(":gliding-soar-service"))
