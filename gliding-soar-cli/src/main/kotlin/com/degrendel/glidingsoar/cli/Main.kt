08d4bcaf4c35895916ca36338b3f05e06f717eb5
Bryan DeGrendel <bryan@degrendel.com>
2019-12-14 13:54:54 -0500

    Add recursive source behavior
    Plus improve checking of inputs against the target bundle output.

--- gliding-soar-cli/src/main/kotlin/com/degrendel/glidingsoar/cli/Main.kt
+++ gliding-soar-cli/src/main/kotlin/com/degrendel/glidingsoar/cli/Main.kt
@@ -8,6 +8,8 @@ import picocli.CommandLine.Command
 import picocli.CommandLine.Option
 import picocli.CommandLine.Parameters
 import java.io.File
+import java.net.URI
+import java.nio.file.Files
 import java.util.concurrent.Callable
 import kotlin.system.exitProcess
 
@@ -19,10 +21,10 @@ class Main(private val args: Array<String>) : Callable<Int>
     val L by logger()
   }
 
-  @Option(names = ["--file-extensions"], description = ["Limit discovered files to the following extensions, separated by commas"], defaultValue = "soar,tcl", split = ",")
-  private var fileExtensions: List<File> = mutableListOf()
+  @Option(names = ["--file-extensions"], description = ["Limit discovered files to the following extensions, separated by commas"], defaultValue = "soar,tcl,glide", split = ",")
+  private var fileExtensions: List<String> = mutableListOf()
 
-  @Option(names = ["--ignore-hidden-files", "-i"], description = ["Ignore hidden files and directories, i.e. any that start with a period"])
+  @Option(names = ["--ignore-hidden-files", "-i"], description = ["Ignore hidden files and directories, i.e. starts with period on UNIX or marked hidden on Windows"])
   private var ignoreHiddenFiles = true
 
   @Option(names = ["--recursive", "-r"], description = ["Recurse into subdirectories"])
@@ -34,26 +36,42 @@ class Main(private val args: Array<String>) : Callable<Int>
   @Parameters(paramLabel = "target", description = ["List of files to parse or directories to search"], arity = "1..*")
   private var targets: List<File> = mutableListOf()
 
+  // NOTE: Ideally would use Files.isSameFile, but that throws an exception if the file is missing
+  // Java! \o/
+  private fun isOutput(outputFile: URI?, file: File): Boolean =
+    (outputFile == file.toPath().toAbsolutePath().normalize().toUri())
+
   override fun call(): Int
   {
     L.info("Running Glide CLI version {}", Version.VERSION)
+    val outputUri = outputFile?.toPath()?.toAbsolutePath()?.normalize()?.toUri()
     val model = ModelImpl(args)
     targets.forEach {
-      if (!it.exists())
+      if (isOutput(outputUri, it))
+      {
+        L.error("Target {} matches the output file!", it)
+        return -1
+      }
+      else if (!it.exists())
       {
         L.error("Target {} does not exist!", it)
         return -1
       }
       else if (it.isDirectory)
       {
-        // Step 1: glob all with extensions in fileExtensions, ignore hidden if ignoreHiddenFiles
-        // Step 2: if recursiveMode, recursive into all directories, ignore hidden if ignoredHiddenFiles
-        TODO("Directories are not yet implemented!")
+        val ignoreHidden = ignoreHiddenFiles
+        val iterator = if (recursiveMode)
+          it.walk().asSequence()
+        else
+          it.listFiles()!!.asSequence()
+        iterator.filter { f -> f.isFile }
+            .filter { f -> !ignoreHidden || !f.isHidden }
+            .filterNot { f -> isOutput(outputUri, f) }
+            .filter { f -> fileExtensions.isEmpty() || fileExtensions.contains(f.extension.toLowerCase()) }
+            .forEach { f -> model.parseFile(f.toURI()) }
       }
       else if (it.isFile)
-      {
         model.parseFile(it.toURI())
-      }
       else
         throw IllegalStateException("Received target $it that exists() but is not isDirectory nor isFile (?)")
     }
